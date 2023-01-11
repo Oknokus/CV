78843de8d74b528bfc824d27311cea443f47cfc7
Oknokus <oknokus@gmail.com>
2023-01-12 01:51:48 +0300

    created CV part3

--- script.js
+++ script.js
@@ -1,11 +1,68 @@
+// SCROLL HEADER
 const headerSame = () => {
-    const header = document.querySelector(`.header__compound`);
-    window.onscroll = () => {
-      if (window.scrollY > 50) {
-        header.classList.add('header__compound-active');
-      } else {
-        header.classList.remove('header__compound-active');
-      }
-    };
+  const header = document.querySelector(`.sideBar__compound`);
+  window.onscroll = () => {
+    if (window.scrollY > 50) {
+      header.classList.add('sideBar__compound-active');
+    } else {
+      header.classList.remove('sideBar__compound-active');
+    }
   };
-  headerSame();
+};
+headerSame();
+
+
+// (function() {
+//   const smoothScroll = function(anchor, duration) {
+//     const headerElHeight = document.querySelector('.header').clientHeight;
+//     let target = document.getElementById(anchor);
+//     let targetPosition = target.getBoundingClientRect().top - headerElHeight;
+//     let startPosition = window.scrollY;
+//     let startTime = null;
+
+//     const ease = function(t, b, c, d) {
+//       t /= d / 1;
+//       if (t < 1) return c / 2 * t * t + b;
+//       t--;
+//       return -c / 2 * (t * (t - 2) - 1) + b;
+//     };
+
+//     const animation = function(currentTime) {
+//       if (startTime === null) startTime = currentTime;
+//       const timeElapsed = currentTime - startTime;
+//       const run = ease(timeElapsed, startPosition, targetPosition, duration);
+//       window.scrollTo(0, run);
+//       if (timeElapsed < duration) requestAnimationFrame(animation);
+//     };
+//     requestAnimationFrame(animation);
+
+//   };
+
+//   const scrollTo = function() {
+//     const links = document.querySelectorAll('.js-scroll');
+//     links.forEach(link => {
+//       link.addEventListener('click', function() {
+
+//         const currentTarget = link.getAttribute('href');
+//         if (!currentTarget) {
+//           return
+//         }
+//         const [link, anchor] = currentTarget.split('#')
+
+//         if (anchor) {
+//           smoothScroll(anchor, 500);
+//         }
+//       });
+//     });
+//   };
+//   scrollTo();
+//   window.addEventListener('load', () => {
+//     const anchor = document.getElementById('container');
+//     if (anchor) {
+//       smoothScroll(anchor, 500);
+//     }
+//   })
+// }());
+
+
+// window.scrollY
