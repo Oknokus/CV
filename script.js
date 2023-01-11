9cc0eab9c6bef805256fd6640e929e0cfdbe1377
Oknokus <oknokus@gmail.com>
2023-01-12 02:28:25 +0300

    created CV part4

--- script.js
+++ script.js
@@ -1,68 +1,72 @@
 // SCROLL HEADER
-const headerSame = () => {
-  const header = document.querySelector(`.sideBar__compound`);
+const sideBarSame = () => {
+  const sideBar = document.querySelector(`.sideBar__compound`);
   window.onscroll = () => {
     if (window.scrollY > 50) {
-      header.classList.add('sideBar__compound-active');
+      sideBar.classList.add('sideBar__compound-active');
     } else {
-      header.classList.remove('sideBar__compound-active');
+      sideBar.classList.remove('sideBar__compound-active');
     }
   };
 };
-headerSame();
+
+sideBarSame();
 
 
-// (function() {
-//   const smoothScroll = function(anchor, duration) {
-//     const headerElHeight = document.querySelector('.header').clientHeight;
-//     let target = document.getElementById(anchor);
-//     let targetPosition = target.getBoundingClientRect().top - headerElHeight;
-//     let startPosition = window.scrollY;
-//     let startTime = null;
+(function() {
+  const smoothScroll = function(anchor, duration) {
+    const headerElHeight = document.querySelector('.sideBar').clientHeight;
+    let target = document.querySelectorAll('#anchor');    
+    let targetPosition =  target.getBoundingClientRect().top - headerElHeight;
+    let startPosition = window.scrollY;
+    let startTime = null;
 
-//     const ease = function(t, b, c, d) {
-//       t /= d / 1;
-//       if (t < 1) return c / 2 * t * t + b;
-//       t--;
-//       return -c / 2 * (t * (t - 2) - 1) + b;
-//     };
+    const ease = function(t, b, c, d) {
+      t /= d / 1;
+      if (t < 1) return c / 2 * t * t + b;
+      t--;
+      return -c / 2 * (t * (t - 2) - 1) + b;
+    };
 
-//     const animation = function(currentTime) {
-//       if (startTime === null) startTime = currentTime;
-//       const timeElapsed = currentTime - startTime;
-//       const run = ease(timeElapsed, startPosition, targetPosition, duration);
-//       window.scrollTo(0, run);
-//       if (timeElapsed < duration) requestAnimationFrame(animation);
-//     };
-//     requestAnimationFrame(animation);
+    const animation = function(currentTime) {
+      if (startTime) startTime = currentTime;
+      const timeElapsed = currentTime - startTime;
+      const run = ease(timeElapsed, startPosition, targetPosition, duration);
+      window.scrollTo(0, run);
+      if (timeElapsed < duration) requestAnimationFrame(animation);
+    };
+    requestAnimationFrame(animation);
 
-//   };
+  };
 
-//   const scrollTo = function() {
-//     const links = document.querySelectorAll('.js-scroll');
-//     links.forEach(link => {
-//       link.addEventListener('click', function() {
+  const scrollTo = function() {
+    const links = document.querySelectorAll('.link-toGo_same');
+ 
+    links.forEach(link => {    
+      link.addEventListener('click', function()  {       
+        const currentTarget = link.getAttribute('href');
+       
 
-//         const currentTarget = link.getAttribute('href');
-//         if (!currentTarget) {
-//           return
-//         }
-//         const [link, anchor] = currentTarget.split('#')
+        if (!currentTarget) {
+          return
+        }
 
-//         if (anchor) {
-//           smoothScroll(anchor, 500);
-//         }
-//       });
-//     });
-//   };
-//   scrollTo();
-//   window.addEventListener('load', () => {
-//     const anchor = document.getElementById('container');
-//     if (anchor) {
-//       smoothScroll(anchor, 500);
-//     }
-//   })
-// }());
+        const [link, anchor] = currentTarget.split('#')
+
+        if (anchor) {
+          smoothScroll(anchor, 500);
+        }
+      });
+    });
+  };
+  scrollTo();
+  window.addEventListener('load', () => {
+    const anchor = document.querySelector('#anchor');
+    if (anchor) {
+      smoothScroll(anchor, 500);
+    }
+  })
+}());
 
 
-// window.scrollY
+window.scrollY
